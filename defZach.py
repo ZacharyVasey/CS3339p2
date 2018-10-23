ff95d6cfd941a54f4cdd27f214f36679dd43a3e9
zwv2 <zwv2@txstate.edu>
2018-10-23 14:32:02 -0500

    zachs def

--- defZach.py
+++ defZach.py
@@ -151,94 +151,15 @@ def sub_one_from_bin(number = []):
 			index = -1
 		index = index - 1
 ####################################################################
-##################################################################################
-#   Simulator Class
-#       Runs assembley code
-##################################################################################
-class Simulator:
-	def ADD(self, arg1, arg2, arg3):
-		arg1 = arg2 + arg3
-	def ADDI(self, arg1, arg2, imm):
-		arg1 = arg2 + imm
-	def SUB(self, arg1, arg2, arg3):
-		arg1 = arg2 - arg3
-	def SUBI(self, arg1, arg2, imm):
-		arg1 = arg2 - imm
-	def LSL(self, arg1, arg2, shift):
-		arg1 = arg2 * (2 ** shift)
-		#can shift be negative?6
-	def LSR(self, arg1, arg2, shift):
-		arg1 = arg2 / (2 ** shift)
-		#can shift be negative?
-	def AND(self, arg1, arg2, arg3):
-		arg2bin = dec_to_sinbin32(arg2)
-		arg3bin = dec_to_sinbin32(arg3)
-		#TESTPRINT
-		print arg2bin
-		print arg3bin
-		arg1bin = []
-		count = 0
-		while(count < len(arg2bin)):
-			if(arg2bin[count] == "1" and arg3bin[count] == "1"):
-				arg1bin.append("1")
-			else:
-				arg1bin.append("0")
-			count = count + 1
-		arg1 = sinbin_to_dec(arg1bin)
-		print arg1
-		print arg1bin
-	def ORR(self, arg1, arg2, arg3):
-		arg2bin = dec_to_sinbin32(arg2)
-		arg3bin = dec_to_sinbin32(arg3)
-		#TESTPRINT
-		print arg2bin
-		print arg3bin
-		arg1bin = []
-		count = 0
-		while(count < len(arg2bin)):
-			if(arg2bin[count] == "1" or arg3bin[count] == "1"):
-				arg1bin.append("1")
-			else:
-				arg1bin.append("0")
-			count = count + 1
-		arg1 = sinbin_to_dec(arg1bin)
-		print arg1
-		print arg1bin
-	def EOR(self, arg1, arg2, arg3):
-		#Code
-		print hello
-	def LDUR(self, arg1, arg2, mem):
-		#Code
-		print hello
-	def SDUR(self, arg1, arg2, mem):
-		#Code
-		print hello
-	def CBZ(self, arg1, offset):
-		#Code
-		print hello
-	def CBNZ(self, arg1, offset):
-		#Code
-		print hello
-	def MOVZ(self, arg1, val, shift):
-		#Code
-		print hello
-	def MOVK(self, arg1, val, shift):
-		#Code
-		print hello
-	def B(self, arg1):
-		#Code
-		print hello
-	def NOP():
-		#Code
-		print hello
-##############################################
-def main():
-	a = Simulator()
-	b = 15
-	c = 20
-	a.ADD(b, c, 10)
-	a.AND(b, 10, 15)
-	a.ORR(b, 10, 15)
+print "54 in binary is 110110: ", dec_to_unsbin(54)
+print "-54 in signed binary is 11111111111111111111111111001010: ", dec_to_sinbin32(-54)
+print "54 in signed binary is 00000000000000000000000000110110: ", dec_to_sinbin32(54)
+print "1 in binary is 1: ", dec_to_unsbin(1)
+print "-1 in signed binary is 11111111111111111111111111111111: ", dec_to_sinbin32(-1)
+print "1 in signed binary is 00000000000000000000000000000001: ", dec_to_sinbin32(1)
 
-if __name__ == "__main__":
-	main()
+print "54 or 0000000000110110 is: ", sinbin_to_dec("0000000000110110")
+print "-106 or 1111111110010110 is: ", sinbin_to_dec("1111111110010110")
+
+print "0 or 0000000000000000 is: ", sinbin_to_dec("0000000000000000")
+print "-1 or 1111111111111111 is: ", sinbin_to_dec("1111111111111111")
